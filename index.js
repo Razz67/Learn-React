d6ef9ccc7f9b9652d3079eb3ac62de4b36ea34b7
Stephine Rasberry <srasberry00@gmail.com>
2022-07-24 21:38:07 -0400

    Complete Challenge Part 1

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 // My first Custom Component
-function SamplePage() {
+// function SamplePage() {
+// 	return (
+// 		<div margin="20px">
+// 			<img src="React.webp" width="10%" />
+// 			<h1 fontWeight="bold">Fun facts about React</h1>
+// 			<ul>
+// 				<li>Was first released in 2013</li>
+// 				<li>Was originally created by Jordan Walke</li>
+// 				<li>Has well over 100K stars on Github</li>
+// 				<li>Is maintained by Facebook</li>
+// 				<li>Powers thousands of enterprixe apps, including mobile apps</li>
+// 			</ul>
+// 		</div>
+// 	);
+// }
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<SamplePage />);
+
+/**
+ * Challenge:
+ * Part 1: Create a page of your own usin a custom Page component
+ *
+ * it should return an ordered list with ethe reasons why you're excited about learning React
+ *
+ * Render your list to the page
+ */
+
+function Page() {
 	return (
-		<div>
-			<img src="./React.webp" width="5%" />
-			<h1 style={{ fontWeighstylet: "bold" }}>Fun facts about React</h1>
-			<ul>
-				<li>Was first released in 2013</li>
-				<li>Was originally created by Jordan Walke</li>
-				<li>Has well over 100K stars on Github</li>
-				<li>Is maintained by Facebook</li>
-				<li>Powers thousands of enterprixe apps, including mobile apps</li>
-			</ul>
-		</div>
+		<ol>
+			<li>It's fun</li>
+			<li>It's easy to learn</li>
+			<li>It's hireable</li>
+		</ol>
 	);
 }
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(SamplePage());
+const list = ReactDOM.createRoot(document.getElementById("root"));
+list.render(<Page />);
