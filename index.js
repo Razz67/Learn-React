9048b91ce455ef75c566cec5b1c0b233e93b7a4c
Stephine Rasberry <srasberry00@gmail.com>
2022-07-24 21:50:25 -0400

    Complete part 2 of Challenge

--- index.js
+++ index.js
@@ -24,15 +24,37 @@
  * it should return an ordered list with ethe reasons why you're excited about learning React
  *
  * Render your list to the page
+ * 
+ * Challenge:
+ * Part 2:
+ * - Add a 'header' element with a nested 'nav' element.  Inside the 'nav',
+ * include a 'img' element with the React log inside (src="React.webp") and
+ * make sure to set the width to something more manageable so it doesn't take
+ * up the whole screen
+ * - Add an 'h1' with some text describing the page. (E.g. "Reasons I'm excited
+ * to learn React"). Place it above the ordered list.
+ * - add a 'footer' after the list that says:
+ *    "copyright 2022 <last name> development.  All rights reserved."
  */
 
 function Page() {
 	return (
-		<ol>
-			<li>It's fun</li>
-			<li>It's easy to learn</li>
-			<li>It's hireable</li>
-		</ol>
+		<div>
+			<header>
+				<nav>
+					<img width="10%" src="React.webp" />
+				</nav>
+			</header>
+			<h1>Reasons I'm excited about learning React</h1>
+			<ol>
+				<li>It's fun</li>
+				<li>It's easy to learn</li>
+				<li>It's hireable</li>
+			</ol>
+			<footer>
+				&copy; 2022 Stephine Rasberry Development.  All rights reserved.
+			</footer>
+		</div>
 	);
 }
 const list = ReactDOM.createRoot(document.getElementById("root"));
